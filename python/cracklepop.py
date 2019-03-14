1ed4c6a8f11acd31cf2c4b9370c20d419bac4550
manila <manila@users.noreply.github.com>
2019-03-14 15:21:07 -0600

    Added Python 3 solution

---
+++ python/cracklepop.py
@@ -0,0 +1,10 @@
+for num in range(1,101):
+	if num % 3 == 0 and num % 5 == 0:
+		print("CracklePop");
+	elif num % 3 == 0:
+		print("Crackle");
+	elif num % 5 == 0:
+		print("Pop");
+	else:
+		print(num);
+
